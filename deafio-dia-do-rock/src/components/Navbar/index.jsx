f1d7321e8d48f75126788781341ea758a9cc3d42
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-07-14 12:15:31 -0300

    update path to Route

--- deafio-dia-do-rock/src/components/Navbar/index.jsx
+++ deafio-dia-do-rock/src/components/Navbar/index.jsx
@@ -31,12 +31,12 @@ const Navbar = () => {
                         </a>
                     </div>
                     <div className="mt-8">
-                        <Link onClick={expandedMenu} to="/route1" className="flex items-center py-2 text-primary">
+                        <Link onClick={expandedMenu} to="/search" className="flex items-center py-2 text-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
                         </Link>
-                        <Link to="/route2" className="flex items-center mt-5 py-2 text-primary">
+                        <Link to="/map" className="flex items-center mt-5 py-2 text-primary">
                             <svg onClick={toggleExpansion} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
                             </svg>
@@ -54,7 +54,7 @@ const Navbar = () => {
                                 }
                             </button>
                         </div>
-                        <Link onClick={expandedMenu} to="/route3" className="flex items-center mt-5 py-2 text-primary">
+                        <Link onClick={expandedMenu} to="/add-envent" className="flex items-center mt-5 py-2 text-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
@@ -65,13 +65,13 @@ const Navbar = () => {
                     {isExpanded && (
                         <div className={`navbar ${darkMode ? 'bg-dark-100 text-light' : 'bg-light text-dark'} w-96 h-screen overflow-auto`}>
                             <Routes>
-                                <Route path="/route1" element={
+                                <Route path="/search" element={
                                     <div className='p-4  items-center py-10 text-primary '>Seu conteúdo para a rota do search aqui!</div>
                                 } />
-                                <Route path="/route2" element={
+                                <Route path="/map" element={
                                     <div className='p-4 items-center py-10 text-primary '>Seu conteúdo para a rota MAPA aqui!</div>
                                 } />
-                                <Route path="/route3" element={
+                                <Route path="/add-envent" element={
                                     <div className='p-4  items-center py-10 text-primary '>Seu conteúdo para a rota Formulario aqui!</div>
                                 } />
                             </Routes>
