fb934d7ed003242c3b8160ba6e013ad066c3aec5
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-07-14 10:59:32 -0300

    chore: refactoring dark mode button

--- deafio-dia-do-rock/src/components/Navbar/index.jsx
+++ deafio-dia-do-rock/src/components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import { DarkModeContext } from '../DarkModeProvider/index';
 
 const Navbar = () => {
     const [isExpanded, setIsExpanded] = useState(false);
-    const { darkMode } = useContext(DarkModeContext);
+    const { darkMode, setDarkMode } = useContext(DarkModeContext);
 
     const toggleExpansion = () => {
         setIsExpanded(!isExpanded);
@@ -15,6 +15,10 @@ const Navbar = () => {
         if (!isExpanded) {
             setIsExpanded(true);
         }
+    };
+
+    const toggleMode = () => {
+        setDarkMode(!darkMode);
     };
 
     return (
@@ -33,25 +37,30 @@ const Navbar = () => {
                             </svg>
                         </Link>
                         <Link to="/route2" className="flex items-center mt-5 py-2 text-primary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <svg onClick={toggleExpansion} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
                             </svg>
                         </Link>
-                        <button onClick={toggleExpansion} className="absolute bottom-0 mb-2 self-end mr-2 mt-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500">
-                                {isExpanded ? (
-                                    <path className='text-red-500' strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                                ) : (
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                                )}
-                            </svg>
-                        </button>
+                        <div className={`${darkMode ? 'bg-dark' : 'bg-light'} absolute bottom-0 mb-2 self-end mr-2 mt-2`}>
+                            <button onClick={toggleMode} className={`${darkMode ? 'text-yellow-600' : 'text-yellow-600'} text-dark top-0 right-0 z-10`}>
+                                {darkMode ?
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
+                                    </svg>
+                                    :
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+                                    </svg>
+                                }
+                            </button>
+                        </div>
                         <Link onClick={expandedMenu} to="/route3" className="flex items-center mt-5 py-2 text-primary">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                         </Link>
                     </div>
+
                 </nav>
                 <div className={`overflow-auto transition-all duration-500 ease-in-out ${isExpanded ? 'w-96' : 'w-0'}`}>
                     {isExpanded && (
