ede15aede72bef4e13883b9ceff7d31bb660167f
Vinidamiap <vinidamiaop@gmail.com>
2024-07-16 19:58:51 -0300

    fixed map first load to use data from db

--- deafio-dia-do-rock/src/App.js
+++ deafio-dia-do-rock/src/App.js
@@ -1,18 +1,30 @@
-import { useContext } from 'react';
+import {useContext, useEffect, useState} from 'react';
 import { DarkModeContext } from './components/DarkModeProvider';
 import './App.css';
 import Navbar from './components/Navbar';
 import Map from "./components/map";
-import dbjson from './db.json';
+import {getAll} from "./Api/eventoApi";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+  const [eventos, setEventos] = useState([]);
+
+  async function getEventos() {
+      try{
+          const data = await getAll();
+          setEventos(data);
+      } catch (error){}
+  }
+
+    useEffect(() => {
+        getEventos();
+    }, []);
 
   return (
     <div className={`flex ${darkMode ? 'dark' : ''}`}>
       <Navbar />
       <div className='flex flex-col w-full'>
-        <Map zoom={4} markers={dbjson} center={{ lat: -22.977112, lng: -43.396305 }} />
+        <Map zoom={4} markers={eventos} center={{ lat: -22.977112, lng: -43.396305 }} />
       </div >
 
     </div >
