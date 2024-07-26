b81417a34687740540ec3b042c57cbdd6428a63a
Angelo Avelino <angeloavelino33211781@gmail.com>
2024-07-25 23:40:25 -0300

    feat: fix api key send blue

---
+++ src/DesafioDiaDoRock.ApplicationCore/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using DesafioDiaDoRock.ApplicationCore.Entities;
+using DesafioDiaDoRock.ApplicationCore.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDiaDoRock.ApplicationCore.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task<bool> SendSendblue(User user, String body, String assunto);
+        Task SendEmailForAlUser(Event @event);
+    }
+}
